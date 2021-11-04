e2f6eaa6b4d983a8d704abcab8a4e701426382f4
Robert3035 <roberthayssalyers@gmail.com>
2021-11-04 08:52:42 -0500

    mid commit

--- list.h
+++ list.h
@@ -1,16 +1,19 @@
 struct Node{
-    char val;
+    int val;
     Node* next;
+    Node* prev;
 
     Node(){
         this->val = '0';
         this->next = nullptr;
+        this->prev = nullptr;
     }
-    Node(char v){
+    Node(int v){
         this->val = v;
         this->next = nullptr;
+        this->prev = nullptr;
     }
-    Node(char v, Node n){
+    Node(int v, Node n){
         this->val = v;
         this->next = &n;
     }
@@ -19,22 +22,25 @@ struct Node{
 class List{
     private:
         Node* front; //points to the front of list
+        Node* back; //points to the back of the list
         Node* it; //points to the current node
         int size; // gives size
         bool empty;
     public:
         List(){
-            this->front = new Node('[');
+            this->front;
+            this->back;
             this->it = front;
             this->size = 0;
         }
-        List(char v){
+        List(int v){
             Node n(v);
             this->front = &n;
+            this->back = &n;
             this->it = &n;
             this->size = 1;
         }
-        char get_iterator(){//Return the value at the iterator
+        int get_iterator(){//Return the value at the iterator
             if(it == nullptr){
                 return ']';
             }
@@ -54,21 +60,30 @@ class List{
                 it = it -> next;
             }
         }
-        char getFront(){//Returns the value that front is pointing at
-            return front -> val;
+        int getFront(){//Returns the value that front is pointing at
+            return front->val;
         }
-        void push_front(char x){//Creates a new node at the beginning of the list
+        int getBack(){//Returns the value that back is pointing at
+            return back->val;
+        }
+        void push_front(int x){//Creates a new node at the beginning of the list
             Node temp(x, *front);
             front = &temp;
+            size++;
+        }
+        void push_back(int x){
+            Node temp(x, *back);
+            back = &temp;
             size++;
         }
         void pop_front(){//Clears the node at the front of the list
             Node *temp = front;
             front = front->next;
+            front->prev = nullptr;
             delete temp;
         }
-        void insert_after(char x){
-            //Created new node, and temp pointer to help us insert it
+        void insert_after(int x){
+            //Creates new node and temp pointer to help us insert it
             if(it->next != nullptr){
                 Node* temp = new Node(x);
                 temp->next = it->next;
@@ -82,12 +97,37 @@ class List{
             }
             size++;
         }
+        void insert(int x){
+            it = front;
+            while(it->next != nullptr){
+                if(x > it->val){
+                    it = it->next;
+                }
+                else if(x < it->val){
+                    Node* temp = new Node(x);
+                    temp->prev = it->prev;
+                    temp->next = it;
+                    it->prev = temp;
+                    it = temp->prev;
+                    it->next = temp;
+                    it = temp;
+                }
+            }
+        }
         void clear(){//Clears the list
             front = nullptr;
             it = nullptr;
-            //front and the iterator don't point at anything, losing all of the nodes and clearing the list
+            back = nullptr;
+            //All pointers don't point at anything, losing all of the nodes and clearing the list
         }
         void move_up(){//Moves the iterator up in the list
-            it = it -> next;
+            if(it->next != nullptr){
+                it = it -> next;
+            }
+        }
+        void move_back(){//Moves the iterator back in the list
+            if(it->prev != nullptr){
+                it = it->prev;
+            }
         }
 };
---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "list.h"
+#include <fstream>
+
+using namespace std;
+
+int main(){
+    List beiju;
+    ifstream fin;
+    fin.open("input2");
+    ofstream fout;
+    fout.open("outfile");
+    
+    fout.close();
+    fin.close();
+}
